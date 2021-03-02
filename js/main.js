f16e5c0a1f3cbe6089c8ef6933ed29e07eac3d76
lperez <lperez@hugeinc.com>
2021-03-01 23:33:30 -0500

    Almost ready, TBD make it reload when window resize

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let chars;
-let masterTl;
+let timeLine;
 let subtitleOverflowVisible = false;
 
 const init = () => {
@@ -37,36 +37,58 @@ const init = () => {
     charsGroups.push(chars.slice(index, index + 3));
   }
 
-  
+  charsGroups.forEach((group, groupIndex) => {
+    group.forEach((char, index) => {
+      let orientation = index % 3 === 0 ? -1 : 1;
+      if (groupIndex === 1 && index === 1) {
+        orientation = -1;
+      } 
+      const maxAngle = (index - 1) % 3 === 0 ? 15 : 25;
+      const minAngle = (index - 1) % 3 === 0 ? 10 : 15;
+      const angle = Math.max(Math.random() * maxAngle, minAngle) * orientation; // max rotation angle 6deg
+      const translation = Math.random() * 20 * orientation; // max translation value 12px
+      char.dataset['collisionData'] = `{ "angle": ${angle.toFixed(2)}, "x": ${translation.toFixed(2)} }`;
+    })
+  }); 
 
 
 
   circle.style.left = `${circleFinalX}px`;
 
 
-  masterTl = gsap.timeline({
+  timeLine = gsap.timeline({
     scrollTrigger: {
       trigger: ".subtitle",
       start: "top top", // when the top of the trigger hits the top of the viewport
       pin: '.subtitle',
-      end: '+=3500',
+      end: '+=5000',
       scrub: 1.5, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
       // markers: true,
     },
     onUpdate: timelineUpdate,
   });
 
+  
 
+  timeLine.to('.hero', { y: '-100%', duration: 8});
+  timeLine.to('#circle', { duration: 8, y: circleTargetY }, "-=8");
+  timeLine.fromTo('#circle', { scale: scaleToApply }, { duration: 10.3, scale: 0}, "-=8");
+  timeLine.to('#circle', { duration: 0.3, opacity: 0 }, "-=0.2");
+  timeLine.from('.subtitle h2 .char', { opacity: 0, y: '120%', stagger: 0.8, duration: 5 }, '-=9.5');
+  timeLine.from('.subtitle .column', { y: '50vh', stagger: 3, duration: 9, ease: 'linear' });
 
-  masterTl.to('.hero', { y: '-100%', duration: 8});
-  masterTl.to('#circle', { duration: 8, y: circleTargetY }, "-=8");
-  masterTl.fromTo('#circle', { scale: scaleToApply }, { duration: 10.3, scale: 0}, "-=8");
-  masterTl.to('#circle', { duration: 0.3, opacity: 0 }, "-=0.2");
-  masterTl.from('.subtitle h2 .char', { opacity: 0, y: '120%', stagger: 0.8, duration: 5 }, '-=9.5');
-  masterTl.fromTo('.subtitle .column', { y: '50vh', stagger: 3, duration: 9 }, { y: '-50vh', stagger: 3, duration: 9 });
-  masterTl.to(charsGroups[0], { y: '-75vh', duration: 9}, '-=12.35');
-  masterTl.to(charsGroups[1], { y: '-75vh', duration: 9}, '-=9.35');
-  masterTl.to(charsGroups[2], { y: '-75vh', duration: 9 }, '-=6.35');
+  chars.forEach((char, index) => {
+    const { angle, x } = JSON.parse(char.dataset.collisionData);
+    const stagger = 3;
+    const animation = { y: '-80vh', rotationZ: angle, x: x, duration: 9 };
+    const factor = index % stagger === 0 ? 0 : 3;
+    const delay = 6 + factor;
+
+    timeLine.to(char, animation, `-=${delay}`)
+  });
+
+  timeLine.to('.subtitle .column', { y: '-155vh', stagger: 3, duration: 21 }, '-=15');
+  timeLine.to('.mario', { opacity: 1, duration: 8 }, '-=13');
 }
 
 const warmUp = () => {
@@ -82,9 +104,9 @@ const warmUp = () => {
 
 const timelineUpdate = () => {
   const animationTreshold = 0.42;
-  if (masterTl.progress() > animationTreshold && !subtitleOverflowVisible) {
+  if (timeLine.progress() > animationTreshold && !subtitleOverflowVisible) {
     toggleSubtitleOverflow(true);
-  } else if (masterTl.progress() <= animationTreshold && subtitleOverflowVisible) {
+  } else if (timeLine.progress() <= animationTreshold && subtitleOverflowVisible) {
     toggleSubtitleOverflow(false);
   }
 };
@@ -93,5 +115,9 @@ const toggleSubtitleOverflow = (makeItVisible) => {
   document.querySelector('.subtitle h2').style.overflow = makeItVisible ? 'visible' : 'hidden';
   subtitleOverflowVisible = makeItVisible;
 };
+
+const getRandomAngle = () => {
+
+}
 
 warmUp(); 
