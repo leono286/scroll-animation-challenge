6fd356d3483c736f58515a3b1ec697392d3a1d50
lperez <lperez@hugeinc.com>
2021-03-11 09:47:36 -0500

    fully responsive now

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-let chars;
-let timeline;
+let splittedText, timeline, vw, vh, scrollTrigger, scrollDuration;
 let subtitleOverflowVisible = false;
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+let processingResize = false;
+let fisrtLoad = true;
 
-const init = () => {
+const setUpTransitions = () => {
   
-  chars = chars[0].chars;
-  
+  const chars = splittedText[0].chars;
+    
   // i char processing to find x coordinate for the dot of the i letter.
   const firstI = chars.filter(char => char.dataset['char'] === 'i')[0];
   const firstIRect = firstI.getBoundingClientRect();
   const firstICenterX = firstIRect.x + firstIRect.width / 2;
   const customFixDistance = 0.0113 * firstICenterX;
+
 
   // Position the center of the circle in the same x coordinate value that the dot of the i letter has. 
   const circle = document.getElementById('circle');
@@ -20,13 +20,15 @@ const init = () => {
   let circleFinalX = firstICenterX - circleRect.width / 2 - customFixDistance;
   circle.style.left = `${circleFinalX}px`;
 
+
   // Find the scale to apply to the circle to make it grow up to 90% of viewport width, no matter the screen size.
   const scaleToApply = vw * 0.9 / 500;
 
-  
+
   // get the required translation of the circle over the y axis to align its center with the center of the dot over the i.
   // 33% is ~ the value meassured from the top of the span's box to the center of the dot. % to ensure responsiveness.
   const circleTargetY = firstIRect.y + (0.33 * firstIRect.height) - circleRect.y - circleRect.width / 2;
+
 
   // process the text to find the bottom line of the text and make it set as top position for the grid that contains the columns
   const subtitle = document.querySelector('.subtitle h2');
@@ -48,31 +50,32 @@ const init = () => {
       if (groupIndex === 1 && index === 1) {
         orientation = -1;
       } 
-      const maxAngle = (index - 1) % 3 === 0 ? 15 : 25;
-      const minAngle = (index - 1) % 3 === 0 ? 10 : 15;
+      const maxAngle = (index - 1) % 3 === 0 ? 20 : 35;
+      const minAngle = (index - 1) % 3 === 0 ? 15 : 20;
       const angle = Math.max(Math.random() * maxAngle, minAngle) * orientation; // max rotation angle 6deg
-      const translation = Math.random() * 20 * orientation; // max translation value 12px
+      const translation = Math.random() * 25 * orientation; // max translation value 12px
       char.dataset['collisionData'] = `{ "angle": ${angle.toFixed(2)}, "x": ${translation.toFixed(2)} }`;
     })
   }); 
 
+  
   //Set up the timeline
-  const scrollDuration = vh * 4;
-
   timeline = gsap.timeline({
     scrollTrigger: {
+      id: 'scrollTriggerId',
       trigger: ".subtitle",
       start: "top top", // when the top of the trigger hits the top of the viewport
       pin: '.subtitle',
       end: `+=${scrollDuration}`,
       scrub: 1.5, // smooth scrubbing, takes 1.5 second to "catch up" to the scrollbar
+      onUpdate: self =>  {scrollTriggerProgress = self.progress}
       // markers: true,
     },
     onUpdate: timelineUpdate,
   });
 
   
-  timeline.to('.hero', { y: '-100%', duration: 8});
+  timeline.to('.hero', { y: '-100%', duration: 8, id: 'first'});
   timeline.fromTo('#circle', {y: '30vh'}, { duration: 8, y: circleTargetY }, "-=8");
   timeline.fromTo('#circle', { scale: scaleToApply }, { duration: 10.3, scale: 0}, "-=8");
   timeline.to('#circle', { duration: 0.3, opacity: 0 }, "-=0.2");
@@ -105,7 +108,12 @@ const init = () => {
 
   timeline.to('.subtitle .column', { y: `-${subtitleVisualBottom + vh}`, stagger: 2, duration: 23 }, '-=13');
   timeline.to('.goomba', { opacity: 1, duration: 8 }, '-=14');
+
+  fisrtLoad && showLoading(false);
 }
+
+
+
 
 const timelineUpdate = () => {
   const animationTreshold = 0.3;
@@ -116,20 +124,102 @@ const timelineUpdate = () => {
   }
 };
 
+
+
+
+
 const toggleSubtitleOverflow = (makeItVisible) => {
   document.querySelector('.subtitle h2').style.overflow = makeItVisible ? 'visible' : 'hidden';
   subtitleOverflowVisible = makeItVisible;
 };
 
-const warmUp = () => {
-  chars = Splitting({
+
+
+
+const init = async () => {
+  vw = document.documentElement.clientWidth;
+  vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+  scrollDuration = vh * 4;
+  splittedText = Splitting({
     target: "[data-splitting]",
     by: "chars",
   });
   gsap.registerPlugin(ScrollTrigger);
-  setTimeout(() => {
-    init();
-  }, 350);
+  await sleep(150);
+  setUpTransitions();
+
+  window.addEventListener('resize', () => { !processingResize && shouldReset()})
 }
 
-warmUp(); 
+
+
+
+const shouldReset = () => {
+  const vwCurrent = document.documentElement.clientWidth;
+  Math.abs(vwCurrent - vw) > 10 && reload() ;
+}
+
+
+
+
+const resetTextSplitted = () => {
+  document.querySelector('h2[data-splitting]').outerHTML = '<h2 data-splitting>Alignment</h2>';
+}
+
+
+
+
+const reload = async () => {
+  processingResize = true;
+  showLoading(true);
+  resetTextSplitted();  
+  const scrollTrigger = ScrollTrigger.getById("scrollTriggerId");
+  const currentProgress = scrollTrigger.progress;
+  scrollTrigger.scroll(0);
+  timeline.pause(0).kill(true);
+  scrollTrigger.kill(true);
+
+  const itemsToReset = ['.hero', '#circle', '.subtitle .column']
+  itemsToReset.forEach(item => { gsap.set(item, { clearProps: true }) });
+
+  init();
+  await sleep(300);
+  
+  const newScrollTrigger = ScrollTrigger.getById("scrollTriggerId");
+  const targetScroll = currentProgress * scrollDuration;
+  timeline.progress(currentProgress);
+  newScrollTrigger.scroll(targetScroll);
+  await sleep(400);
+  timelineUpdate();
+  processingResize = false;
+  showLoading(false);
+}
+
+
+
+
+
+const showLoading = (show) => {
+  const body = document.body;
+  if (show) {
+    body.classList.add('loading');
+  } else {
+    body.classList.remove('loading');
+  }
+}
+
+
+
+
+
+const sleep = t => new Promise(resolve => setTimeout(resolve, t));
+
+
+
+
+
+init(); 
+
+
+//instantiate GSDevTools with default settings
+// GSDevTools.create();
